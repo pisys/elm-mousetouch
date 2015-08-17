c9240b65514fb0a21eeb428285384658b8de83fc
Matthias <dev@pisys.eu>
2015-08-17 10:53:37 +0200

    introduced TapBox a
    a variant of a Mailbox which exposes click/tap handler instead of
    address, and signal a. TapBox wraps taps and yields "a".

--- Tap.elm
+++ Tap.elm
@@ -1,36 +1,50 @@
-module Tap (onClick, onClickWithOptions, tapSignal) where
+module Tap (TapBox,tapbox) where
 import Html exposing (Attribute, text, Html, div)
 import Html.Events exposing (onWithOptions, Options)
 import Json.Decode as Json
 import Debug
 
-onClickWithOptions : Options -> Signal.Address a -> a -> List Attribute
-onClickWithOptions options address msg =
-    let helper event a = 
-        onWithOptions event options Json.value (\_ -> Signal.message touches.address a)
-    in
-    [helper "touchstart" (Start msg)
-    ,helper "touchmove" (Cancel msg)
-    ,helper "touchleave" (Cancel msg)
-    ,helper "touchend" (End msg)
-    ]
+type alias TapBox a = {
+        onClick : (a -> List Attribute),
+        onClickWithOptions : (Options -> a -> List Attribute),
+        signal : Signal a
+    }
 
-onClick = onClickWithOptions {stopPropagation = False, preventDefault = False}
+tapbox : a -> TapBox a
+tapbox noop = 
+    let mailbox = touches noop
 
-touches : Signal.Mailbox (Tap a)
-touches =
+        onClickWithOptions options msg =
+            let 
+                helper event x = 
+                    onWithOptions event options Json.value (\_ -> Signal.message mailbox.address x)
+            in
+                [helper "mousedown" (Start msg)
+                ,helper "mousemove" (Cancel msg)
+                ,helper "touchleave" (Cancel msg)
+                ,helper "mouseup" (End msg)
+                ]
+
+    in 
+        {
+            onClickWithOptions = onClickWithOptions,
+            onClick = onClickWithOptions {stopPropagation = False, preventDefault = False},
+            signal = Signal.filterMap touchAction noop (Signal.foldp touchesToAction init mailbox.signal)
+        }
+
+
+touches : a -> Signal.Mailbox (Tap a)
+touches a =
     Signal.mailbox NoTap
 
-init : a -> TapModel a
-init start = {
-    noop = start,
-    pending = start,
+init : TapModel a
+init = {
+    pending = Nothing,
     carryOut = False
     }
 
 type alias TapModel a = {
-    noop : a,
-    pending : a,
+    pending : Maybe a,
     carryOut : Bool
     }
 
@@ -42,62 +56,17 @@ touchesToAction tap model =
     case (Debug.log "tap" tap) of 
         NoTap -> model
         Start act -> 
-            { model | pending <- act, carryOut <- False }
+            { model | pending <- Just act, carryOut <- False }
         End act ->
-            if model.pending == act then 
+            if model.pending == Just act then 
                 { model | carryOut <- True }
             else
-                init model.noop
+                init 
         Cancel act ->
-            init model.noop
+            init 
 
-touchModel : a -> Signal (TapModel a)
-touchModel a =
-    Signal.foldp touchesToAction (init a) touches.signal
-
-touchAction : TapModel a -> a
+touchAction : TapModel a -> Maybe a
 touchAction model =
     if model.carryOut then (Debug.log "touchAction" model.pending)
-    else (Debug.log "touchAction" model.noop)
-
-type Action = NoOp | Do Int
-
-type alias Model = {
-    one : Int,
-    two : Int,
-    three : Int
-    } 
-
-update: Action -> Model -> Model
-update act model = 
-    case act of
-        NoOp -> model
-        Do i -> 
-            if | i == 1 -> {model| one <- model.one + 1}
-               | i == 2 -> {model| two <- model.two + 1 }
-               | i == 3 -> {model| three <- model.three + 1}
-            
-
-mailbox : Signal.Mailbox Action
-mailbox = 
-    Signal.mailbox NoOp
-
-view : Signal.Address Action -> Model -> Html
-view address model =
-    div [] 
-        [ div (onClick address (Do 1)) [text (toString model.one)]
-        , div (onClick address (Do 2)) [text (toString model.two)]
-        , div (onClick address (Do 3)) [text (toString model.three)]
-        ]
-
-tapSignal : a -> Signal a
-tapSignal a =
-    Signal.map touchAction (touchModel a)
-
-model : Signal Model
-model = Signal.merge mailbox.signal (tapSignal NoOp)
-    |> Signal.foldp update {one=0,two=0,three=0}
-
-main = 
-    Signal.map (view mailbox.address) model
+    else (Debug.log "touchAction" Nothing)
 
