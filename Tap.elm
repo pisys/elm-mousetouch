8b01b0e83cec1d6b7545ee0cbc5fa1f69d8b3a91
Matthias <dev@pisys.eu>
2015-08-20 08:37:00 +0200

    refactor event string to smaller form

--- Tap.elm
+++ Tap.elm
@@ -3,7 +3,7 @@ import Html exposing (Attribute, text, Html, div)
 import Html.Events 
 import Json.Decode as Json
 import Debug
-import Time
+import Time exposing (Time, millisecond, timestamp, second)
 import Regex exposing (..)
 import String
 
@@ -42,7 +42,7 @@ tapbox noop =
                 Signal.filterMap parseAction noop 
                     <| Signal.map parseInput
                     <| Signal.foldp updateModel init 
-                    <| Time.timestamp mailbox.signal
+                    <| timestamp mailbox.signal
         }
 
 type Device = Mouse | Touch
@@ -55,19 +55,21 @@ touches a =
 init : TapModel a
 init = {
     pastEvents = "",
-    success = Nothing
+    success = Nothing,
+    timebase = 0
     }
 
-type alias Event = ((Device, Action), Time.Time)
+type alias Event = ((Device, Action), Time)
 
 type alias PastEvents = String
 
 type alias TapModel a =
     { pastEvents : PastEvents
     , success : Maybe ((PastEvents -> Bool), a)
+    , timebase : Time
     }
 
-updateModel : (Time.Time, Maybe (((Device, Action), (PastEvents -> Bool)), a)) -> TapModel a -> TapModel a
+updateModel : (Time, Maybe (((Device, Action), (PastEvents -> Bool)), a)) -> TapModel a -> TapModel a
 updateModel tap model =
     let t = fst tap
         e = snd tap
@@ -75,23 +77,35 @@ updateModel tap model =
     case e of 
         Nothing -> model
         Just ev -> 
-            let newEvent = toString (fst (fst ev), t)
+            let newEvent = eventToString (fst (fst ev), t)
                 success = (snd (fst ev), snd ev)
 
                 -- throw away events which are older than one second
-                newPastEvents = throwAwaySince Time.second <| Debug.log "adjustTime" (adjustTime t model.pastEvents)
+                newPastEvents = throwAwaySince second <| Debug.log "adjustTime" (adjustTime t model.pastEvents)
             in 
                { model | pastEvents <- (Debug.log "newEvent" newEvent ) ++ (Debug.log "pastEvents" newPastEvents)
                        , success <- Just success
                }
 
-throwAwaySince : Time.Time -> PastEvents -> PastEvents
+eventToString : Event -> String
+eventToString ((device,action),time) =
+    (deviceToString device) ++ (actionToString action) ++ (toString time)
+
+deviceToString : Device -> String
+deviceToString device =
+    toString device |> String.left 1 
+
+actionToString : Action -> String
+actionToString action =
+    toString action |> String.left 1 |> String.toLower 
+
+throwAwaySince : Time -> PastEvents -> PastEvents
 throwAwaySince since events =
-    let es = find All (regex regexAnyEvent) events
+    let es = find All (regex (Debug.log "throwAway, regexAnyEvent" regexAnyEvent)) events
     in .string <| List.foldl (keepBefore since) {string = "", cum = 0} es
     --fst (List.partition (\(_,t') -> t' > (now - since)) list)
 
-keepBefore : Time.Time -> Match -> {string: String, cum : Float} -> {string:String, cum: Float}
+keepBefore : Time -> Match -> {string: String, cum : Float} -> {string:String, cum: Float}
 keepBefore since match past =
     {-- string = if past.cum > since then "true" else "false"
     --, cum = if past.cum > since then 0 else 1
@@ -111,22 +125,43 @@ keepBefore since match past =
     , cum = cum
     }
 
-adjustTime : Time.Time -> PastEvents -> PastEvents
+adjustTime : Time -> PastEvents -> PastEvents
 adjustTime now events =
     replace (AtMost 1) (regex "\\d+") (\{match} -> replaceByDiff now (Debug.log "adjustTime, match" match)) events
 
-replaceByDiff : Time.Time -> String -> String
+replaceByDiff : Time -> String -> String
 replaceByDiff now time =
     case String.toFloat (Debug.log "replacebydiff, time =" time) of
         Err x -> "0"
         Ok t -> toString (now-t)
+
+buildRegexFromString : String -> String -> String
+buildRegexFromString device action =
+    device ++ action ++ "(\\d+)"
         
-buildRegex : String -> String -> String
+buildRegex : List Device -> List Action -> String
 buildRegex device action =
-    "\\(\\(" ++ device ++ "," ++ action ++ "\\),(\\d+)\\)"
+        (buildRegexDevice device)
+        ++ (buildRegexAction action)
+        ++ "(\\d+)"
+
+buildRegexDevice : List Device -> String
+buildRegexDevice device =
+    let b = if List.length device > 1 then ("[","]") else ("","")
+    in (fst b) 
+        ++ (List.foldl (\d -> \dd -> dd ++ deviceToString d) "" device) 
+        ++ (snd b) 
+
+buildRegexAction : List Action -> String
+buildRegexAction action =
+    let b = if List.length action > 1 then ("[","]") else ("","")
+    in (fst b)
+        ++ (List.foldl (\a -> \aa -> aa ++ actionToString a) "" action) 
+        ++ (snd b) 
+
 
 regexAnyEvent : String
-regexAnyEvent = buildRegex (dontremember regexAnyDevice) (dontremember regexAnyAction)
+regexAnyEvent = buildRegexFromString (dontremember regexAnyDevice) (dontremember regexAnyAction)
 
 dontremember : String -> String
 dontremember string = "(?:"++string++")"
@@ -135,16 +170,16 @@ remember : String -> String
 remember string = "("++string++")"
 
 regexAnyMouseEvent : String
-regexAnyMouseEvent = buildRegex "Mouse" regexAnyAction
+regexAnyMouseEvent = buildRegex [Mouse] [Start,End,Leave,Move]
 
 regexAnyTouchEvent : String
-regexAnyTouchEvent = buildRegex "Touch" regexAnyAction
+regexAnyTouchEvent = buildRegex [Touch] [Start,End,Leave,Move]
 
 regexAnyAction : String
-regexAnyAction = "Start|End|Leave|Move"
+regexAnyAction = buildRegexAction [Start,End,Leave,Move] 
 
 regexAnyDevice : String
-regexAnyDevice = "Mouse|Touch"
+regexAnyDevice = buildRegexDevice [Mouse,Touch]
 
 parseInput : TapModel a -> TapModel a
 parseInput model = 
@@ -155,7 +190,7 @@ parseInput model =
 twoStarts : PastEvents -> PastEvents
 twoStarts events =
     replace (AtMost 1) (
-        "(" ++ (buildRegex regexAnyDevice "Start") ++ "){2}" |> regex
+        "(" ++ (buildRegex [Mouse,Touch] [Start]) ++ "){2}" |> regex
     ) (\m -> (Debug.log "match" m).match ) events
 
 parseAction : TapModel a -> Maybe a
@@ -167,20 +202,20 @@ parseAction model =
                then Just (snd s)
                else Nothing
 
-click : Time.Time -> Time.Time -> PastEvents -> Bool
+click : Time -> Time -> PastEvents -> Bool
 click range recent events =
     let matchTouch = Debug.log "touchclick head" 
             <| find (AtMost 1) (regex
                     <| "^"
-                    ++ buildRegex "Touch" "End" 
-                    ++ buildRegex "Touch" "Start"
+                    ++ buildRegex [Touch] [End] 
+                    ++ buildRegex [Touch] [Start]
                 )
                 events
         matchMouse = Debug.log "mouseclick head" 
             <| find (AtMost 1) (regex
                     <| "^"
-                    ++ buildRegex "Mouse" "End" 
-                    ++ buildRegex "Mouse" "Start"
+                    ++ buildRegex [Mouse] [End]
+                    ++ buildRegex [Mouse] [Start]
                 )
                 events
         diff = 
@@ -190,10 +225,10 @@ click range recent events =
 
         last =
             if List.isEmpty matchTouch
-               then getLast "Touch" events
+               then getLast (deviceToString Touch) events
                else Nothing --getLast "Mouse" events
 
-        getDiff : List Match -> Maybe Time.Time
+        getDiff : List Match -> Maybe Time
         getDiff matches =
             case List.head matches of
                 Nothing -> Nothing
@@ -220,9 +255,9 @@ click range recent events =
                l > recent
                -- means: at least 300ms after a mouse event (or vice-versa)
         
-getLast : String -> PastEvents -> Maybe Time.Time
+getLast : String -> PastEvents -> Maybe Time
 getLast device events =
-    let es = find All (regex <| buildRegex (remember regexAnyDevice) (dontremember regexAnyAction)) events
+    let es = find All (regex <| buildRegexFromString (remember regexAnyDevice) (dontremember regexAnyAction)) events
             |> List.drop 1 -- drop the first event since it contains the absolute timestamp
     in case List.foldl (accumDiffs device) (Just {cum=0, stop=False}) (Debug.log "getLast events" es) of
         Nothing -> Nothing
@@ -232,7 +267,7 @@ getLast device events =
                else Nothing
 
 
-accumDiffs : String -> Match -> Maybe {cum : Time.Time, stop: Bool} -> Maybe {cum : Time.Time, stop: Bool}
+accumDiffs : String -> Match -> Maybe {cum : Time, stop: Bool} -> Maybe {cum : Time, stop: Bool}
 accumDiffs stopat match cum =
     case cum of
         Nothing -> Nothing
